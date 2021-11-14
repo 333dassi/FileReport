1e5d8562db80382da283fe38a53f06b54b683690
333dassi <86553835+333dassi@users.noreply.github.com>
2021-11-14 15:18:42 +0200

    upload file

---
+++ Hadasim_homeTest/FileDataReport/FileDataReport/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace FileDataReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The report:");
+            Read read = new Read();
+            read.Reporting();
+
+        }
+    }
+}
